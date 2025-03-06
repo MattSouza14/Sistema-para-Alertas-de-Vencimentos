2966ecf39bce97477955461c2daf1eb13fc9c5b4
MattSouza14 <mateussouzabr1@gmail.com>
2025-03-06 17:36:04 -0300

    sql ideias novas

--- src/db_teste/DB_teste.sql
+++ src/db_teste/DB_teste.sql
@@ -13,3 +13,9 @@ dataVencimento DATE NOT NULL
 
 INSERT INTO  ficha (nome, email, dataVencimento) VALUES ('Mateus Souza', 'mateussouzabr1@gmail.com', '2026-02-24');
 SELECT nome AS 'Nome do Usúario', email AS 'Email de Cadastro', date_format(dataVencimento, '%d/%m/%Y') AS 'DATA VENCIMENTO OFFICE' FROM ficha;
+
+-- Fazer select  SELECT * FROM ficha_cadastro
+-- Fazer select  SELECT dataVencimento FROM ficha_Cadastro order By desc
+-- Fazer um delete para um id especifico (usar WHERE)
+-- Alterar o campo dataVencimento de um id especifico 
+-- Criar um novo registro com nome, email, data vencimento 
